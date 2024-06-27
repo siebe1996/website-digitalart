1f9d0b475cdd3ba49ce2783b3656e14fa5bd4574
siebe van de voorde <siebe.vandevoorde@student.odisee.be>
2024-06-27 11:50:43 +0200

    changed backend to localhost

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 export const APP_TITLE = "DigitalArt";
-export const BACKEND_API2 =
-    "https://d641-2001-818-e61b-8400-98b2-9269-6205-5978.ngrok-free.app/api";
+export const BACKEND_API = "https://edf7-193-136-56-49.ngrok-free.app/api";
 export const BACKEND_URL = "https://test-eps-porto3.azurewebsites.net/";
-export const BACKEND_API = BACKEND_URL + "api/";
+export const BACKEND_API2 = BACKEND_URL + "api/";
 export const FRONTEND_URL = "http://localhost:8081/";
